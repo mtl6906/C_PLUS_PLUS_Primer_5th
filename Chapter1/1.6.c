974824d98fb8f61c1980892845505ab93a6248fd
mtl6906 <zzz5486129@163.com>
2018-05-26 16:20:50 +0800

    Create 1.6.c

---
+++ Chapter1/1.6.c
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+        int v1,v2;
+
+        std::cout << "Enter two numbers:" << std::endl;
+        std::cin >> v1 >> v2;
+
+/*
+        std::cout << "The sum of " << v1;
+                  << " and" << v2;
+                  << " is " << v1 + v2 << std::endl;
+        不合法
+*/
+
+        std::cout << "The sum of " << v1
+                  << " and " << v2
+                  << " is " << v1 + v2 << std::endl;
+
+        return 0;
+}
