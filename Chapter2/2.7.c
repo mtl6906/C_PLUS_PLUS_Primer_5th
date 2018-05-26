e3b81c36598ab32a79c5329ec53282958498ee3d
mtl6906 <zzz5486129@163.com>
2018-05-26 21:09:24 +0800

    Create 2.7.c

---
+++ Chapter2/2.7.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+        std::cout << "Who goes with F\145rgus?\012" << std::endl;       //      string
+
+        std::cout << 3.14e1L << std::endl;      //      long double 
+
+//      std::cout << 1024f << std::endl;                error
+
+        std::cout << 3.14L << std::endl;        //      long double
+
+        return 0;
+}
