9fd574b7a8aba3ccd0cbc2a51235a3f4eb078703
mtl6906 <zzz5486129@163.com>
2018-05-26 21:00:48 +0800

    Create 2.6.c

---
+++ Chapter2/2.6.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+        int month = 9,day = 7;
+//      int month1 = 09,day1 = 07;      error
+
+        std::cout << month << day << std::endl
+
+        return 0;
+}
