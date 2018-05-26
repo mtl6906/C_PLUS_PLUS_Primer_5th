b98f007cae5952af03725776e893da231afe968f
mtl6906 <zzz5486129@163.com>
2018-05-26 19:28:26 +0800

    Create 1.21.c

---
+++ Chapter1/1.21.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "1/Sales_item.h"
+
+int main()
+{
+        Sales_item si1,si2;
+
+        std::cin >> si1 >> si2;
+        std::cout << si1 + si2 << std::endl;
+
+        return 0;
+}
