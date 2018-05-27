14400f3887e595ba08fdfa325b98a6b6ac8e64ff
mtl6906 <zzz5486129@163.com>
2018-05-27 14:09:05 +0800

    Create 2.14.c

---
+++ Chapter2/2.14.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+        int i = 100,sum = 0;
+        for(int i=0;i!=10;++i)
+                sum += i;       //      i = 0 to 9
+
+        std::cout << i << " " << sum << std::endl;      //      i=100
+
+        return 0;
+}
