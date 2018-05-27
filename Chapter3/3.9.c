dac9ee4ca9078a9482b078ab5aff070e81e577e6
mtl6906 <zzz5486129@163.com>
2018-05-27 18:46:21 +0800

    Create 3.9.c

---
+++ Chapter3/3.9.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+        string s;
+        cout << s[0] << endl;   //      不合法，未定义
+
+        return 0;
+}
