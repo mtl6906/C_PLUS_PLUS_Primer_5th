ed7f05747f72f6c8a29894f236dca87082f0e325
mtl6906 <zzz5486129@163.com>
2018-05-27 13:45:44 +0800

    Create 2.9.c

---
+++ Chapter2/2.9.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+//      std::cin >> int input_value;
+        int input_value;
+        std::cin >> input_value;
+//      int i = {3.14}
+        int i = 3.14;
+        std::cout << i << std::endl;
+//      double salary = wage = 9999.99;
+        double salary = 9999.99,wage = 9999.99;
+//      int u = 3.14;
+        int u=3.14;
+        std::cout << u << std::endl;
+        return 0;
+}
