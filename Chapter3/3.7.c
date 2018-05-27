9c8cee1e71027634ca85780511af641a2ea01b99
mtl6906 <zzz5486129@163.com>
2018-05-27 18:39:21 +0800

    Create 3.7.c

---
+++ Chapter3/3.7.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+        string s;
+        cin >> s;               //      输入一个字符串
+
+        for(char &c:s)          //      将每个元素转化为'X'
+                c = 'X';
+
+        cout << s << endl;
+
+        return 0;
+}
