44df9768109243e2a174008d8891b647153c6dca
mtl6906 <zzz5486129@163.com>
2018-05-28 08:41:02 +0800

    Create 6.8.c

---
+++ Chapter6/6.8.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Chapter6.h"
+
+using std::cout;
+using std::endl;
+
+int main(void)
+{
+        cout << fact(5) << endl;
+        cout << fact() << endl;
+        cout << abs(-5) << endl;
+
+        return 0;
+}
