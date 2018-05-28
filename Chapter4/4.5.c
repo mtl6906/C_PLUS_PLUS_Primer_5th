cb2e9e04df0d674154df9117cd16246103c8c156
mtl6906 <zzz5486129@163.com>
2018-05-29 07:46:32 +0800

    Create 4.5.c

---
+++ Chapter4/4.5.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+        cout << -30 * 3 + 21 / 5 << endl;
+        cout << -30 + 3 * 21 / 5 << endl;
+        cout << -30 / 3 * 21 % 5 << endl;
+        cout << -30 / 3 * 21 % 4 << endl;
+
+        return 0;
+}
