f43669a53ea9dc2c3ed08fff5bcf2bb2316bda95
mtl6906 <zzz5486129@163.com>
2018-05-29 08:04:21 +0800

    Create 4.10.c

---
+++ Chapter4/4.10.c
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+        int num = 0;
+
+        while(num != 42)
+        {
+                cin >> num;
+                cout << num << endl;
+        }
+
+        return 0;
+}
