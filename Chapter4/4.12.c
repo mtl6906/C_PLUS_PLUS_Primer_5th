417d8a371892bbc245a912076db9076a0a432718
mtl6906 <zzz5486129@163.com>
2018-05-29 08:09:47 +0800

    Create 4.12.c

---
+++ Chapter4/4.12.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+        int i,j,k;
+
+        if(i!=j<k)      //      i!=k返回0或1，如果i!=k的值小于k返回1
+                cout << "yes" << endl;
+        else
+                cout << "no" << endl;
+
+        return 0;
+}
