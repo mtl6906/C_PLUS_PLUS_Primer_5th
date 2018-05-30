decc85294eb7355c07d5763642d1cfd3fa3a9835
mtl6906 <zzz5486129@163.com>
2018-05-30 16:16:50 +0800

    Create 5.4.c

---
+++ Chapter5/5.4.c
@@ -0,0 +1,21 @@
+#include <string>
+
+using std::string;
+
+int main()
+{
+        while(string::iterator iter != e.end()){}       //      当iter不等于e的尾后指针时，循环继续
+
+//      while(bool status = find(word)){}               //      当find(word)返回true时，循环继续
+
+//              if(!status){}   error，外部不可见
+
+        bool status;
+
+        while(status = find(word)){}
+
+        if(!status){}   //
+
+
+        return 0;
+}
