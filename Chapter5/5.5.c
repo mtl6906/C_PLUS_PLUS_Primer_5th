ef814e0083d2ab65076a0867c05fd72120183da7
mtl6906 <zzz5486129@163.com>
2018-05-30 16:31:36 +0800

    Update 5.5.c

--- Chapter5/5.5.c
+++ Chapter5/5.5.c
@@ -13,11 +13,11 @@ int main()
         if(score < 60)
                 cout << "F" << endl;
         else if(score <70)
-                cout << "E" << endl;
-        else if(score < 80)
                 cout << "D" << endl;
-        else if(score < 90)
+        else if(score < 80)
                 cout << "C" << endl;
+        else if(score < 90)
+                cout << "B" << endl;
         else if(score < 100)
                 cout << "A" << endl;
         else
