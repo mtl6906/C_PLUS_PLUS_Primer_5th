1f4f6108b9f11a951d0a4af37e8f9b68ced9c55c
mtl6906 <zzz5486129@163.com>
2018-05-30 20:36:45 +0800

    Create 5.25.c

---
+++ Chapter5/5.25.c
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdexcept>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::runtime_error;
+
+int main()
+{
+        int a,b;
+
+        while(cin >> a >> b)
+        {
+                try
+                {
+                        if(b == 0)
+                                throw runtime_error("second value can't be 0");
+                        cout << a / b << endl;
+                        break;
+                }
+                catch(runtime_error err)
+                {
+                        cout << err.what() << endl;
+                        cout << "Input again: " << endl;
+                }
+        }
+        return 0;
