e5aeed9ad9d4b7f532de2e21b6cd143778ebd59a
mtl6906 <zzz5486129@163.com>
2018-05-31 10:22:00 +0800

    Update 6.44.c

--- Chapter6/6.44.c
+++ Chapter6/6.44.c
@@ -5,13 +5,13 @@ using std::string;
 using std::endl;
 using std::cout;
 
-inline const string& shorterString(const string &s1,const string &s2)
+inline const bool isShorter(const string &s1,const string &s2)
 {
-        return s1.size() <= s2.size() ? s1 : s2;
+        return s1.size() < s2.size();
 }
 
 int main()
 {
-        cout << shorterString("abc","abcd") << endl;
+        cout << isShorter("abc","abcd") << endl;
         return 0;
 }
