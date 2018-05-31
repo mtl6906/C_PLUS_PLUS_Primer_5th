a814a191a83170b0e19855b1581b3acfe6090d5e
mtl6906 <zzz5486129@163.com>
2018-05-31 15:40:00 +0800

    Create 6.51.c

---
+++ Chapter6/6.51.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+void manip(int a,int b)
+{
+
+}
+
+double dobj;
+
+int main()
+{
+        manip('a','z');         //      算术类型的转换
+
+        manip(55.4,dobj);       //      算术类型的转换
+
+        return 0;
+}
