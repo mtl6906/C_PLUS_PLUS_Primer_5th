a63bca39a26e08a5447decdad1644f09d5e2f2c4
mtl6906 <zzz5486129@163.com>
2018-05-31 19:16:32 +0800

    Update Sales_data.c

--- Chapter7/Sales_data.c
+++ Chapter7/Sales_data.c
@@ -1,8 +1,6 @@
 #include "Sales_data.h"
 
-using std::string;
-
-string Sales_data::isbn()const
+std::string Sales_data::isbn()const
 {
         return bookNo;
 }
@@ -12,4 +10,23 @@ Sales_data& Sales_data::combine(const Sales_data& o)
         revenue += o.revenue;
         units_sold += o.units_sold;
         return *this;
+}
+
+Sales_data add(Sales_data& o1,Sales_data& o2)
+{
+        Sales_data sum = o1;
+        sum.revenue += o2.revenue;
+        sum.units_sold += o2.units_sold;
+        return sum;
+}
+
+std::istream read(std::istream &is,Sales_data &o)
+{
+        cin >> o.bookNo >> o.units_sold >> o.revenue;
+        return cin;
+}
+
+std::ostream print(std::ostream &os,Sales_data &o)
+{
+        os << "isbn: " << o.bookNo << ",units_sold: " << o.units_sold << ",revenue: " << o.revenue << endl;
 }
