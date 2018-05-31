e0a90e63be4fa132d4662119e1b0d941630385a5
mtl6906 <zzz5486129@163.com>
2018-05-31 19:38:45 +0800

    Update Person.h

--- Chapter7/Person.h
+++ Chapter7/Person.h
@@ -1,11 +1,14 @@
 #ifndef PERSON_H
 #define PERSON_H
 #include <string>
+#include <iostream>
 struct Person
 {
         std::string name;
         std::string address;
         std::string getName()const;
         std::string getAddress()const;
+        std::istream& read(std::istream &,Person &);
+        std::ostream& print(std::ostream &,const Person &);
 };
 #endif
