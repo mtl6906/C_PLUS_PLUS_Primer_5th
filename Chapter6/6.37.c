b1a57fd7701d21c7744ffe157059034ebb1624b4
mtl6906 <zzz5486129@163.com>
2018-05-31 08:32:34 +0800

    Create 6.37.c

---
+++ Chapter6/6.37.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int odd[10];
+
+typedef int arr[10];
+
+arr* getarray();
+
+decltype(odd)* getarray1();
+
+auto getarray2() -> int[10];
+
+int main()
+{
+
+        return 0;
+}
