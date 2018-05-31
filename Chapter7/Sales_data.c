3fcf443d22f20677671f161c172745457cff379b
mtl6906 <zzz5486129@163.com>
2018-05-31 19:25:31 +0800

    Update Sales_data.c

--- Chapter7/Sales_data.c
+++ Chapter7/Sales_data.c
@@ -12,7 +12,7 @@ Sales_data& Sales_data::combine(const Sales_data& o)
         return *this;
 }
 
-Sales_data add(Sales_data& o1,Sales_data& o2)
+Sales_data add(const Sales_data& o1,const Sales_data& o2)
 {
         Sales_data sum = o1;
         sum.revenue += o2.revenue;
@@ -20,13 +20,14 @@ Sales_data add(Sales_data& o1,Sales_data& o2)
         return sum;
 }
 
-std::istream read(std::istream &is,Sales_data &o)
+std::istream& read(std::istream &is,Sales_data &o)
 {
-        cin >> o.bookNo >> o.units_sold >> o.revenue;
-        return cin;
+        is >> o.bookNo >> o.units_sold >> o.revenue;
+        return is;
 }
 
-std::ostream print(std::ostream &os,Sales_data &o)
+std::ostream& print(std::ostream &os,Sales_data &o)
 {
-        os << "isbn: " << o.bookNo << ",units_sold: " << o.units_sold << ",revenue: " << o.revenue << endl;
+        os << "isbn: " << o.bookNo << ",units_sold: " << o.units_sold << ",revenue: " << o.revenue << std::endl;
+        return os;
 }
