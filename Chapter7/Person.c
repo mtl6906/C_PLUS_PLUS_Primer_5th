0188e2e4b3719e885d305a565e3ac1d453d03db3
mtl6906 <zzz5486129@163.com>
2018-05-31 19:43:27 +0800

    Update Person.c

--- Chapter7/Person.c
+++ Chapter7/Person.c
@@ -10,12 +10,13 @@ std::string Person::getAddress() const
         return address;
 }
 
-std::istream& Person::read(std::istream &is,Person &o)
+
+std::istream& read(std::istream &is,Person &o)
 {
         return is >> name >> address;
 }
 
-std::ostream& Person::print(std::ostream &os,const Person &o)
+std::ostream& print(std::ostream &os,const Person &o)
 {
         return os << "name: " << name << ",address: " << address << std::endl;
 }
