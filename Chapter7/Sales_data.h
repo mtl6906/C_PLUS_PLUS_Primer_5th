2fd3d5a91a38e9aae9e36750ed595b5adc5e8a99
mtl6906 <zzz5486129@163.com>
2018-05-31 19:17:05 +0800

    Update Sales_data.h

--- Chapter7/Sales_data.h
+++ Chapter7/Sales_data.h
@@ -1,6 +1,8 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 #include <string>
+#include <iostream>
+
 struct Sales_data
 {
         std::string bookNo;
@@ -10,4 +12,7 @@ struct Sales_data
         Sales_data& combine(const Sales_data &);
 };
 
+Sales_data add(const Sales_data&,const Sales_data&);
+std::istream &read(std::istream &,Sales_data &);
+std::ostream &print(std::ostream &,Sales_data &);
 #endif
