cd6f87916b51b70ecbd4e05a296cec962cb8d2f2
mtl6906 <zzz5486129@163.com>
2018-06-01 20:22:25 +0800

    Update Sales_data.c

--- Chapter7/Sales_data.c
+++ Chapter7/Sales_data.c
@@ -1,5 +1,10 @@
 #include "Sales_data.h"
 
+inline double avg_price()
+{
+        return revenue / units_sold;
+}
+
 Sales_data::Sales_data(const std::string &s)
 {
         bookNo = s;
@@ -30,4 +35,14 @@ Sales_data add(const Sales_data& o1,const Sales_data& o2)
         sum.revenue += o2.revenue;
         sum.units_sold += o2.units_sold;
         return sum;
+}
+
+std::istream& read(std::istream &is,Sales_data &o)
+{
+        return is >> o.bookNo >> o.units_sold >> o.revenue;
+}
+
+std::ostream& print(std::ostream &os,const Sales_data &o)
+{
+        return os << "isbn: " << o.bookNo << ",units_sold: " << o.units_sold << ",revenue: " << o.revenue << std::endl;
 }
