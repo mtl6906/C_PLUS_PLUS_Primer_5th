150470ecc109c8f92f9522a94e42aef15b93cc4f
mtl6906 <zzz5486129@163.com>
2018-06-01 19:37:53 +0800

    Update Person.h

--- Chapter7/Person.h
+++ Chapter7/Person.h
@@ -4,13 +4,19 @@
 #include <iostream>
 struct Person
 {
-        std::string name;
-        std::string address;
-        std::string getName()const;
-        std::string getAddress()const;
+        friend std::istream& read(std::istream &,Person &);
+        friend std::ostream& print(std::ostream &,const Person &);
+
+        private:
+                std::string name;
+                std::string address;
+        public:
+                std::Person() = default;
+                std::Person(const std::string &,const std::string &);
+                std::string getName()const;
+                std::string getAddress()const;
 };
 
 std::istream& read(std::istream &,Person &);
 std::ostream& print(std::ostream &,const Person &);
 #endif
-
