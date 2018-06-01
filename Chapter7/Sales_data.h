bccd4af11784807b7d23708ee89a1bcfa9ffddf9
mtl6906 <zzz5486129@163.com>
2018-06-01 19:34:30 +0800

    Update Sales_data.h

--- Chapter7/Sales_data.h
+++ Chapter7/Sales_data.h
@@ -6,15 +6,21 @@
 
 struct Sales_data
 {
-        std::string bookNo;
-        unsigned int units_sold = 0;
-        double revenue = 0.0;
-        Sales_data() = default;
-        Sales_data(const std::string &);
-        Sales_data(const std::string &,unsigned,double);
-        Sales_data(std::istream &is){std::cin >> bookNo >> units_sold >> revenue;}
-        std::string isbn()const;
-        Sales_data& combine(const Sales_data &);
+        friend Sales_data add(const Sales_data&,const Sales_data&);
+        friend std::istream &read(std::istream &,Sales_data &);
+        friend std::ostream &print(std::ostream &,const Sales_data &);
+
+        private:
+                std::string bookNo;
+                unsigned int units_sold = 0;
+                double revenue = 0.0;
+        public:
+                Sales_data() = default;
+                Sales_data(const std::string &);
+                Sales_data(const std::string &,unsigned,double);
+                Sales_data(std::istream &is){std::cin >> bookNo >> units_sold >> revenue;}
+                std::string isbn()const;
+                Sales_data& combine(const Sales_data &);
 };
 
 Sales_data add(const Sales_data&,const Sales_data&);
