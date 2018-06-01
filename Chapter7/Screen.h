34838b9c19e10b57547e79589f49831b3d8b3875
mtl6906 <zzz5486129@163.com>
2018-06-01 21:37:39 +0800

    Create Screen.h

---
+++ Chapter7/Screen.h
@@ -0,0 +1,69 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+#include <string>
+#include <iostream>
+class Screen
+{
+	public:
+		typedef std::string::size_type pos;
+		Screen() = default;
+		Screen(pos ht,pos wd) : height(ht),width(wd),content(ht*wd,' '){};
+		Screen(pos ht,pos wd,char c) : height(ht),width(wd),content(ht*wd,c){};
+		char get() const;
+		char get(pos,pos) const;
+		Screen &move(pos,pos);
+		Screen &set(char);
+		Screen &set(pos,pos,char);
+		Screen &display(std::ostream&);
+		const Screen &display(std::ostream&)const;
+	private:
+		pos height = 0,width = 0;	
+		pos cursor = 0;		
+		std::string content;	
+		void do_display(std::ostream&)const;	
+};
+
+inline char Screen::get() const
+{
+	return content[cursor];
+}
+
+inline char Screen::get(pos r,pos c)const
+{
+	return content[r*width+c];
+}
+
+inline Screen &Screen::move(pos r,pos c)
+{
+	cursor = r * width + c;
+	return *this;
+}
+
+inline Screen &Screen::set(char ch)
+{
+	content[cursor] = ch;
+	return *this;		
+}
+
+inline Screen &Screen::set(pos r,pos c,char ch)
+{
+	content[r*width+c];
+	return *this;
+}	
+
+inline void Screen::do_display(std::ostream &os) const
+{
+	os << content;
+}
+
+inline Screen& Screen::display(std::ostream &os)
+{
+	do_display(os);
+	return *this;
+}
+inline const Screen& Screen::display(std::ostream &os) const
+{
+	do_display(os);
+	return *this;
+}
+#endif
