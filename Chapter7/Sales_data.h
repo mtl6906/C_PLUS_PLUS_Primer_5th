c5354dc92001fed7ca47c9af646cda03abf03687
mtl6906 <zzz5486129@163.com>
2018-06-01 20:22:59 +0800

    Update Sales_data.h

--- Chapter7/Sales_data.h
+++ Chapter7/Sales_data.h
@@ -14,6 +14,7 @@ struct Sales_data
                 std::string bookNo;
                 unsigned int units_sold = 0;
                 double revenue = 0.0;
+                double avg_price();
         public:
                 Sales_data() = default;
                 Sales_data(const std::string &);
