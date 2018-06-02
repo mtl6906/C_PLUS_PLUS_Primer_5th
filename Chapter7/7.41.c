729d0debf530cf3f005c312c3178347723df1f0f
mtl6906 <zzz5486129@163.com>
2018-06-02 14:24:42 +0800

    Create 7.41.c

---
+++ Chapter7/7.41.c
@@ -0,0 +1,15 @@
+#include "Sales_data.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+        Sales_data s1;
+        cout << endl;
+        Sales_data s2("1");
+        cout << endl;
+        Sales_data s3("1",2,3.0);
+        cout << endl;
+        return 0;
+}
