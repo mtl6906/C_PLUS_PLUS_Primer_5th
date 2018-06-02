46b54db45f68a588e9d8dd3a9d2c4797fbe326d7
mtl6906 <zzz5486129@163.com>
2018-06-02 19:15:52 +0800

    Create Example.h

---
+++ Chapter7/Example.h
@@ -0,0 +1,12 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+#include <vector>
+
+class Example
+{
+        public:
+                static double rate;
+                static const int vecSize;
+                static std::vector<double> vec;
+};
+#endif
