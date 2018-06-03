fa88f8ef38cf96449ced0bdb5c3d1d6002a37d4d
mtl6906 <zzz5486129@163.com>
2018-06-03 08:13:05 +0800

    Rename 8.7.c to 8.8.c

---
+++ Chapter8/8.8.c
@@ -0,0 +1,53 @@
+#include "Sales_data.h"
+#include <fstream>
+
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+
+int main(int argc,char **argv)
+{
+        Sales_data total;
+
+        ifstream fin(argv[1]);
+        ofstream fout(argv[2],std::fstream::app);
+
+        if(!fin.is_open())
+        {
+                cerr << argv[1] << " can't open..." << endl;
+                return -1;
+        }
+
+        if(!fout.is_open())
+        {
+                cerr << argv[2] << "can't open..." << endl;
+                return -1;
+        }
+
+        if((total = Sales_data(fin)),fin)
+        {
+                Sales_data cur;
+                while((cur = Sales_data(fin)),fin)
+                {
+                        if(cur.isbn() == total.isbn())
+                        {
+                                total.combine(cur);
+                        }
+                        else
+                        {
+                                print(fout,total);
+                                total = cur;
+                        }
+                }
+                print(fout,total);
+        }
+        else
+        {
+                cerr << "no data..." << endl;
+                return -1;
+        }
+
+        return 0;
+}
