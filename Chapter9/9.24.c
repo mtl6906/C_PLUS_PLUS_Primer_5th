3250f5489bf613dca0e311685a24db553fbd5e86
mtl6906 <zzz5486129@163.com>
2018-06-03 15:49:34 +0800

    Create 9.24.c

---
+++ Chapter9/9.24.c
@@ -0,0 +1,18 @@
+#include <vector>
+
+using std::vector;
+
+int main()
+{
+        vector<int> iv;
+
+        iv.at(0);
+
+        iv[0];
+
+        iv.front();
+
+        *iv.begin();
+
+        return 0;
+}
