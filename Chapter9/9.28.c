f4729067618eb4f4854dc528f700dd5a574c9dcf
mtl6906 <zzz5486129@163.com>
2018-06-04 08:27:23 +0800

    Update 9.28.c

--- Chapter9/9.28.c
+++ Chapter9/9.28.c
@@ -8,16 +8,26 @@ using std::string;
 
 void insert(forward_list<string> &sfl,const string &des,const string &val)
 {
-        for(auto s=sfl.begin();s!=sfl.end();++s)
+        auto s = sfl.begin();
+        auto prev = sfl.before_begin();
+        while(s != sfl.end())
+        {
                 if(*s == des)
+                {
                         sfl.insert_after(s,val);
+                        return;
+                }
+                ++prev;
+                ++s;
+        }
+        sfl.insert_after(prev,val);
 }
 
 int main()
 {
         forward_list<string> sfl = {"a","b","c","d"};
 
-        insert(sfl,"b","e");
+        insert(sfl,"w","e");
 
         for(const auto& s : sfl)
                 cout << s << endl;
