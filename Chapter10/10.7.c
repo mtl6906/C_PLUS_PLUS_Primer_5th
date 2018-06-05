0c8214232319dce4be9c966882f1b0688fcce451
mtl6906 <zzz5486129@163.com>
2018-06-05 17:58:50 +0800

    Update and rename 10.7 to 10.7.c

--- Chapter10/10.7.c
+++ Chapter10/10.7.c
@@ -26,5 +26,14 @@ int main()
         for(const auto & c : iv)
                 cout << c << endl;
 
+        vector<int> vec;
+
+        vec.reserve(10);
+
+        fill_n(vec.begin(),10,0);       //      vec的size还是0
+
+        for(const auto& c : vec)
+                cout << c << endl;
+
         return 0;
 }
