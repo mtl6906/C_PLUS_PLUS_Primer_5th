82997efb29d3ae74d23af371297f480743a17856
mtl6906 <zzz5486129@163.com>
2018-06-06 08:47:35 +0800

    Create 10.19.cpp

---
+++ Chapter10/10.19.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+string make_plural(int count,const string &word,const string &s)
+{
+        return count > 1 ? word + s : word;
+}
+
+void elimDups(vector<string> &words)
+{
+        sort(words.begin(),words.end());
+        for(const auto& c : words)
+                cout << c << " ";
+        cout << endl;
+        words.erase(unique(words.begin(),words.end()),words.end());
+}
+
+void biggies(vector<string> &words,vector<string>::size_type sz)
+{
+        elimDups(words);
+        stable_sort(words.begin(),words.end(),[](const string &a,const string &b){return a.size() < b.size();});
+        auto wc = stable_partition(words.begin(),words.end(),[sz](const string &a){return a.size() >= sz;});
+        auto count = wc - words.begin();
+        cout << count << " " << make_plural(count,"word","s") << " of length " << sz << " or longer " << endl;
+
+        for_each(words.begin(),wc,[](const string &s){cout << s << " ";});
+        cout << endl;
+}
+int main()
+{
+        string s;
+        vector<string> sv;
+        while(cin >> s)
+                sv.push_back(s);
+
+        biggies(sv,5);
+
+        return 0;
+}
