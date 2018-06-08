f0fe642f753ba043a8866aa277ed22fd5f86b35d
mtl6906 <zzz5486129@163.com>
2018-06-08 08:17:53 +0800

    Create 11.8.cpp

---
+++ Chapter11/11.8.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+int main()
+{
+        vector<string> sv;
+
+        string word;
+
+        while(cin >> word)      
+                sv.push_back(word);
+
+        return 0;
+}
