0b9aa5613eae96a04ab574f5a9b7c793e76c4ce5
mtl6906 <zzz5486129@163.com>
2018-06-09 09:39:44 +0800

    Create 11.26.cpp

---
+++ Chapter11/11.26.cpp
@@ -0,0 +1,17 @@
+#include <map>
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::map;
+using std::map;
+using std::pair;
+
+int main()
+{
+        map<string,int> mp;
+
+        int t = (mp["154"] = 5);        //      返回值int
+
+        return 0;
+}
