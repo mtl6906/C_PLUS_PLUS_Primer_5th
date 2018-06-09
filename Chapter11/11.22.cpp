555ec563872f45cc6661b3fec15e43fffd33edb8
mtl6906 <zzz5486129@163.com>
2018-06-09 09:10:07 +0800

    Create 11.22.cpp

---
+++ Chapter11/11.22.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using std::pair;
+using std::map;
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
+using std::vector;
+
+int main()
+{
+        vector<int> iv;
+        string key;
+
+        map<string,vector<int>> mp;
+
+        pair<map<string,vector<int>>::iterator,bool> t = mp.insert({key,iv});
+
+        return 0;
+}
