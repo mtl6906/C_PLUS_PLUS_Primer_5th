f88a75515444e9f022e3f916ed93a203f5e6712a
mtl6906 <zzz5486129@163.com>
2018-06-10 09:36:56 +0800

    Create StrBlob.cpp

---
+++ Chapter12/StrBlob.cpp
@@ -0,0 +1,25 @@
+#include "StrBlob.h"
+
+void StrBlob::pop_back()
+{
+        check(0,"pop_back on empty StrBlob");
+        data -> erase(data -> begin() + data -> size() - 1);
+}
+
+std::string& StrBlob::front()
+{
+        check(0,"front on empty StrBlob");
+        return data -> front();
+}
+
+std::string& StrBlob::back()
+{
+        check(0,"back on empty StrBlob");
+        return data -> back();
+}
+
+void StrBlob::check(size_type i,const std::string &msg) const
+{
+        if(i >= data -> size())
+                throw std::out_of_range(msg);
+}
