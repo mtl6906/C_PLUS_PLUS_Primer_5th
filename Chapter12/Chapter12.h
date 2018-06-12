cd4440a5b31d5d5d6a5b7c8c6e5d89dce5c4d450
mtl6906 <zzz5486129@163.com>
2018-06-12 19:51:08 +0800

    Update Chapter12.h

--- Chapter12/Chapter12.h
+++ Chapter12/Chapter12.h
@@ -1,13 +1,20 @@
 #ifndef CHAPTER12_H
 #define CHAPTER12_H
 
+#include <set>
+#include <memory>
+#include <sstream>
+
 class StrBlob;
 class StrBlobPtr;
 class ConstStrBlobPtr;
+class QueryResult;
+class TextQuery;
 
 #include "StrBlob.h"
 #include "ConstStrBlobPtr.h"
 #include "StrBlobPtr.h"
-
+#include "TextQuery.h"
+#include "QueryResult.h"
 
 #endif
