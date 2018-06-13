2988c754d317d1c543129d42283f69f7808564e6
mtl6906 <zzz5486129@163.com>
2018-06-13 09:25:44 +0800

    Create 13.14.cpp

---
+++ Chapter13/13.14.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class numbered
+{
+        public:
+                numbered()
+                {
+                        static int curr;
+                        mysn = ++curr;
+                }
+                int mysn;
+};
+
+
+void f(numbered s){cout << s.mysn << endl;}
+int main()
+{
+        numbered a,b = a,c = b;
+        f(a);f(b);f(c);
+        return 0;
+}
