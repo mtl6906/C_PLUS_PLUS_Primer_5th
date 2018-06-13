3a842ff8ece8147c725978edeebc1801ae14bc24
mtl6906 <zzz5486129@163.com>
2018-06-13 09:05:34 +0800

    Create 13.11.cpp

---
+++ Chapter13/13.11.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+class HasPtr
+{
+        public:
+                HasPtr(const std::string &s = std::string()) : ps(new std::string(s)),i(0){}
+                HasPtr(const HasPtr& hp) : ps(new std::string(*hp.ps)),i(hp.i){};
+                HasPtr& operator(const HasPtr& hp)
+                {
+                        ps = new string(*hp.ps);
+                        i = hp.i;
+                }
+                ~HasPtr()
+                {
+                        delete ps;
+                }
+        private:
+                std::string *ps;
+                int i;
+};
