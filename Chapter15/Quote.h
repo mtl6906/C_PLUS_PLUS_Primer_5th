14a5fa8a45a0ad1e2fe36a0714d612d3abb021c0
mtl6906 <zzz5486129@163.com>
2018-06-14 09:06:01 +0800

    Create Quote.h

---
+++ Chapter15/Quote.h
@@ -0,0 +1,25 @@
+#ifndef QUOTE
+#define QUOTE
+
+#include <string>
+#include <iostream>
+
+class Quote
+{
+        friend double print_total(std::ostream&,const Quote&,size_t);
+        public:
+                Quote() = default;
+                Quote(const std::string &book,double sales_price) : bookNo(book),price(sales_price){}
+
+                std::string isbn()const {return bookNo;};
+                virtual double net_price(std::size_t n)const{return n * price;}
+                virtual ~Quote() = default;
+        private:
+                std::string bookNo;
+        protected:
+                double price = 0.0;
+};
+
+double print_total(std::ostream&,const Quote&,size_t);
+
+#endif
