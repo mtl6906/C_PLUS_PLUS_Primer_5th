678f593ababecf3645a455ddb51034184ef0710c
mtl6906 <zzz5486129@163.com>
2018-06-14 09:06:26 +0800

    Create Quote.cpp

---
+++ Chapter15/Quote.cpp
@@ -0,0 +1,9 @@
+#include "Quote.cpp"
+
+double print_total(std::ostream& os,const Quote& item,size_t n)
+{
+        double ret = item.net_price(n);
+        os << "ISBN: " << item.isbn() << "# sold: " << n << " total due: "
+           << ret << std::endl;
+        return ret;
+}
